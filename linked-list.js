18d73bd2dc11fd9955e357b05f2748a1de2de547
Celso Espinoza <celso.espinoza@rappi.com>
2022-12-31 07:46:39 -0500

    ADding remove to linkedList

--- linked-list.js
+++ linked-list.js
@@ -30,6 +30,19 @@ class LinkedList {
         return this;
     }
 
+    traverseToIndex(index) {
+        if (index === this.length) {
+            return null;
+        }
+        let counter = 0;
+        let currentNode = this.head;
+        while (counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
+
     insert(index, value) {
         if (index > this.length - 1) {
             return 'Error';
@@ -44,16 +57,37 @@ class LinkedList {
             return this.append(value);
         }
 
-        let nodePreviousIndex = this.head;
-        for (let i = 0; i < index - 1; i++) {
-            nodePreviousIndex = nodePreviousIndex.next;
-        }
+        const nodePreviousIndex = this.traverseToIndex(index - 1);
         const nodeToInsert = new Node(value);
         nodeToInsert.next = nodePreviousIndex.next;
         nodePreviousIndex.next = nodeToInsert;
-
         this.length++;
+        return this;
+    }
 
+    remove(index) {
+        if (index < 0) {
+            return 'Error';
+        }
+        if (index > this.length - 1) {
+            return 'Error';
+        }
+        if (index === 0) {
+            this.head = this.head.next;
+            this.length--;
+            return this;
+        }
+        const beforeIndexNode = this.traverseToIndex(index - 1);
+        const afterIndexNode = this.traverseToIndex(index + 1);
+
+        beforeIndexNode.next = afterIndexNode;
+
+        if (index === this.length - 1) {
+            this.tail = beforeIndexNode;
+        }
+        
+        this.length--;
+        
         return this;
     }
 }
@@ -62,6 +96,11 @@ const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(6);
 myLinkedList.prepend(1);
-myLinkedList.insert(3, 20);
+myLinkedList.insert(2, 20);
 // 1 -> 10 --> 20 -> 5 --> 6
+myLinkedList.append(8);
+myLinkedList.remove(myLinkedList.length - 1);
+// 1 -> 10 --> 20 -> 5 --> 6
+myLinkedList.remove(2);
+// 1 -> 10 --> 5 --> 6
 console.log(JSON.stringify(myLinkedList));
