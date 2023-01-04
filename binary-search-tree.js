e3033c11d28afc215bec9bfa44c498fa4bea0bca
Celso Espinoza <c.espinoza.arce@outlook.com>
2023-01-04 07:43:05 -0500

    Adding binary search tree insert and lookup functions

---
+++ binary-search-tree.js
@@ -0,0 +1,64 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        }
+
+        const { parentNode, sideChosen } = this.findParentNode(value);
+        parentNode[sideChosen] = newNode;
+        return this;
+    }
+
+    lookup(value) {
+        const { parentNode, sideChosen } = this.findParentNode(value);
+        return parentNode[sideChosen];
+    }
+
+    findParentNode(value) {
+        let nodeSearch = this.root;
+        let grandParentNode;
+        let parentNode;
+        let sideChosen;
+
+        while (nodeSearch) {
+            grandParentNode = parentNode;
+            parentNode = nodeSearch;
+            if (value === nodeSearch.value) {
+                return { parentNode: grandParentNode, sideChosen };
+            }
+            if (value > nodeSearch.value) {
+                nodeSearch = nodeSearch.right;
+                sideChosen = 'right';
+            } else {
+                nodeSearch = nodeSearch.left;
+                sideChosen = 'left';
+            }
+        }
+        return { parentNode, sideChosen };
+    }
+}
+
+const myBinarySearchtree = new BinarySearchTree();
+myBinarySearchtree.insert(27);
+myBinarySearchtree.insert(14);
+myBinarySearchtree.insert(52);
+myBinarySearchtree.insert(5);
+myBinarySearchtree.insert(59);
+myBinarySearchtree.insert(50);
+console.log(JSON.stringify(myBinarySearchtree));
+console.log(JSON.stringify(myBinarySearchtree.lookup(52)));
