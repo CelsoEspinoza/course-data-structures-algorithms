4b49dfa58b3dd1540669ffb1d96b8836a1530a0b
Celso Espinoza <c.espinoza.arce@outlook.com>
2023-01-26 07:59:43 -0500

    Knowing Disktra and Bellman-Ford algorithms

--- searching/binary-search.js
+++
@@ -1,3 +0,0 @@
-// Binary search tree is a sorted data structure.
-// Choose the half value and see if it's in left or right.
-// Repeat until you find the target value
--- searching/breadth-first-search.js
+++
@@ -1,4 +0,0 @@
-// Finds the closest path in a tree/graph
-// Takes more memory.
-
-// It goes level by level.
