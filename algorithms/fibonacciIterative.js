e81c01dc2a35b8e5e513b1e8bf66acff7a7a3d32
Celso Espinoza <c.espinoza.arce@outlook.com>
2023-02-01 06:41:14 -0500

    Adding fibonaci with dynamic programming

--- algorithms/fibonacciIterative.js
+++ algorithms/fibonacciIterative.js
@@ -5,4 +5,25 @@ function fibonacciRecursive(index) { // O(2^n) Horrible!!!
     return fibonacciRecursive(index-1) + fibonacciRecursive(index-2);
 }
 
-console.log(fibonacciRecursive(24));
+// console.log(fibonacciRecursive(24));
+
+function dynamicProgrammingFibo() { // O(n)
+    const cache = {};
+    const fibo = (index) => {
+        if (index in cache) {
+            return cache[index];
+        }
+        if (index < 2) {
+            return index;
+        }
+        cache[index] = fibo(index-1) + fibo(index-2);
+        return cache[index];
+    }
+    return fibo;
+}
+
+const fibonacciFunc = dynamicProgrammingFibo();
+
+console.log(fibonacciFunc(50));
+console.log(fibonacciFunc(55));
+console.log(fibonacciFunc(100));
