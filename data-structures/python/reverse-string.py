9dd84c6dcc904240728fedcd176f1e214ecfc1a9
Celso Espinoza <c.espinoza.arce@outlook.com>
2024-04-29 06:40:10 -0500

    add reverse string python

---
+++ data-structures/python/reverse-string.py
@@ -0,0 +1,22 @@
+
+def reverse(str):
+    if not str:
+        return None
+    return str[::-1]
+
+def reverse2(str):
+    reversedWord = []
+    for char in str:
+        reversedWord.insert(0, char)
+    return "".join(reversedWord)
+
+def reverse3(str):
+    if (len(str) == 1):
+        return str
+    
+    return str[len(str) - 1] + reverse3(str[:-1])
+
+print(reverse(''))
+print(reverse('Hola, mi nombre es Celso'))
+print(reverse2('Hola, mi nombre es Celso'))
+print(reverse3('Hola, mi nombre es Celso'))
